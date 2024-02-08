bcbabc178e5848b45f2675419d7d2ddef4b90b77
Gulzat33 <islamainazik2007@gmail.com>
2024-02-09 00:18:56 +0600

    DZ1

--- js/home_works.js
+++ js/home_works.js
@@ -1,5 +1,24 @@
+//workhome1
+
 //Gmail checker
 
+const gmailInput=document.querySelector("#gmail_input")
+const gmailButton=document.querySelector("#gmail_button")
+const gmailResult=document.querySelector("#gmail_result")
+
+const regExp = /^[a-zA-Z0-9._%+-]+@gmail.com$/;
+
+
+gmailButton.addEventListener('click',()=> {
+    const inputText = gmailInput.value
+    if (regExp.test(inputText)) {
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    } else {
+        gmailResult.innerHTML='NOT OK'
+        gmailResult.style.color='red'
+    }
+})
 
 
 
@@ -10,14 +29,7 @@
 
 
 
-
-
-
-
-
-
-
-//
+// часть 2
 const childBlock = document.querySelector(".child_block")
 
 let posX = 0
