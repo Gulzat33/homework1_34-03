e4aead83347a31001866f6ec0bc21aaea5792389
Gulzat33 <islamainazik2007@gmail.com>
2024-02-13 23:17:27 +0600

    homework2

--- js/home_works.js
+++ js/home_works.js
@@ -2,25 +2,23 @@
 
 //Gmail checker
 
-const gmailInput=document.querySelector("#gmail_input")
-const gmailButton=document.querySelector("#gmail_button")
-const gmailResult=document.querySelector("#gmail_result")
-
-const regExp = /^[a-zA-Z0-9._%+-]+@gmail.com$/;
-
-
-gmailButton.addEventListener('click',()=> {
-    const inputText = gmailInput.value
-    if (regExp.test(inputText)) {
-        gmailResult.innerHTML = 'OK'
-        gmailResult.style.color = 'green'
-    } else {
-        gmailResult.innerHTML='NOT OK'
-        gmailResult.style.color='red'
-    }
-})
-
-
+// const gmailInput=document.querySelector("#gmail_input")
+// const gmailButton=document.querySelector("#gmail_button")
+// const gmailResult=document.querySelector("#gmail_result")
+//
+// const regExp = /^[a-zA-Z0-9._%+-]+@gmail.com$/;
+//
+//
+// gmailButton.addEventListener('click',()=> {
+//     const inputText = gmailInput.value
+//     if (regExp.test(inputText)) {
+//         gmailResult.innerHTML = 'OK'
+//         gmailResult.style.color = 'green'
+//     } else {
+//         gmailResult.innerHTML='NOT OK'
+//         gmailResult.style.color='red'
+//     }
+// })
 
 
 
@@ -29,7 +27,9 @@ gmailButton.addEventListener('click',()=> {
 
 
 
-// часть 2
+
+//
+// // часть 2
 const childBlock = document.querySelector(".child_block")
 
 let posX = 0
@@ -40,6 +40,64 @@ const moveBlock = () => {
         posX += 16
         childBlock.style.left = `${posX}px`
         setTimeout(moveBlock, 25)
+    } else if (posX >= 444 && posY <= 440) {
+        posY += 16
+        childBlock.style.top = `${posY}px`
+        setTimeout(moveBlock, 25)
+    } else if (posX > 0 && posY >=440) {
+        posX -= 16
+        childBlock.style.left = `${posX}px`
+        setTimeout(moveBlock, 25)
+    }
+    else {
+        posY -= 16
+        childBlock.style.top = `${posY}px`
+        setTimeout(moveBlock, 25)
     }
 }
 moveBlock()
+
+
+//homework2 // time
+
+const startBtn = document.querySelector('#start')
+const stopBtn = document.querySelector('#stop')
+const resetBtn = document.querySelector('#reset')
+
+
+let newValue = 0;
+let interval;
+let isRunning = false;
+
+
+startBtn.addEventListener('click', ()=>{
+    if (!isRunning) {
+        toIncrease();
+        document.querySelector('.interval').style.color = 'orange'
+        isRunning = true
+    }
+})
+stopBtn.addEventListener('click', ()=>{
+    toStop();
+})
+resetBtn.addEventListener('click', ()=>{
+    toReset()
+    document.querySelector('.interval').style.color = 'yellow'
+})
+const toIncrease = () =>{
+    toStop()
+    interval = setInterval(function (){
+        newValue++
+        document.querySelector('.interval').innerHTML = newValue;
+    }, 1000)
+}
+const toStop = ()=>{
+    clearInterval(interval)
+    isRunning = false
+}
+const toReset = () =>{
+    clearInterval(interval)
+    newValue = 0;
+    document.querySelector('.interval').innerHTML = newValue;
+}
+
--- js/lesson.js
+++ js/lesson.js
@@ -1,2 +1,74 @@
 //PHONE  CHECKER
- const phoneInput = document.querySelector("button")
+ const phoneInput = document.querySelector("input")
+ const phoneButton = document.querySelector("button")
+ const phoneResult = document.querySelector("result")
+
+const regExp =/^\+966[2579]\d {2}\d{2}-\d{2}$/
+
+phoneButton.addEventListener('click',() =>{
+ if (regExp.test(phoneInput)){
+  phoneResult.innerHTML="OK"
+  phoneResult.style.color="green"
+ } else {
+  phoneResult.innerHTML="NO OK"
+  phoneResult.style.color="red"
+ }
+})
+
+
+
+
+
+//Основы осинхронности
+
+//Синхронный код
+// console.log(1)
+// console.log(2)
+// console.log(3)
+//
+// //асинхронный код
+// setTimeout(()=>{
+//  console.log(1)
+// },1000)
+//
+// console.log(2)
+
+
+// const logger =(messege) => {
+//   console.log(messege)
+// }
+//
+// console.log('loading...')
+// setTimeout(() =>{
+//   logger(messegeFromPrompt)
+// },2500)
+//
+//
+// setTimeout(()=>{
+//  console.log('Hi')
+// })
+//
+//
+// const interval =setInterval(() =>{
+//   console.log('Hello')
+// },1000)
+// setTimeout(() =>{
+//  clearInterval(interval)
+// },5000)
+// const secondsBlock=document.querySelector('p')
+// const button=document.querySelector('button')
+// let seconds=0
+// const interval= (()=>{
+//  seconds++
+//  // console,logger(seconds)
+//  document.write(interval)
+// },1000)
+//
+// button.addEventListener('clik',()=>{
+//  clearInterval(interval)
+// })
+
+
+
+Event Loopффф
+//Соартголдтьортпрпои
